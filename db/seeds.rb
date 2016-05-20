4d149e74b1fc5b733bb35f97d658849a62a87095
Victor W <vwalldev@gmail.com>
2016-05-19 21:07:29 -0700

    build basic home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dictum feugiat vestibulum. Aliquam suscipit 
+quam eget dui scelerisque, viverra efficitur lorem hendrerit. Maecenas hendrerit mi sit amet nibh rutrum dapibus."
+
+small_cover_urls = ["/tmp/family_guy.jpg", "/tmp/futurama.jpg", "/tmp/monk.jpg", "/tmp/south_park.jpg"]
+
+["Family Guy", "Family Dude", "Family Bro", "Family Dwarf", "Family Elf", "Family Dark Elf", "Family Girl", "Family Babe", "Family Toddler"].each do |title|
+
+  video = Video.create(title: title, description: description, small_cover_url: small_cover_urls.shuffle.first, large_cover_url: "/tmp/monk_large.jpg")
+end
